a4c29ea2d25ffd6dbb2dab8e14bc2e825f7def50
Fabian Hirter <mail@fabianhirter.ch>
2021-08-25 11:37:12 +0200

    adjust ip lookup

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-public_ip=$(ip a | grep "inet 192.168.56" | cut -d "/" -f 1 | cut -c 10-100)
+public_ip=$(ip a | grep "inet 192.168" | cut -d "/" -f 1 | cut -c 10-100)
 PUBLIC_IP=$public_ip node Backend/index.js
