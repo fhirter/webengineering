8939d03b04b6119c122698900b975b010764bb30
Fabian Hirter <mail@fabianhirter.ch>
2021-08-25 10:42:47 +0200

    improve test: run multiple times

--- src/generateNumbers.test.js
+++ src/generateNumbers.test.js
@@ -1,7 +1,9 @@
 const sum = require('./generateNumbers');
 
 test('generates random integer between 0 and 100', () => {
-    let number = sum.randInt();
-    expect(number).toBeGreaterThanOrEqual(0);
-    expect(number).toBeLessThanOrEqual(100);
+    for (let i = 0; i < 1000; i++) {
+        let number = sum.randInt();
+        expect(number).toBeGreaterThanOrEqual(0);
+        expect(number).toBeLessThanOrEqual(100);
+    }
 });
