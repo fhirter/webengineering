d1bf1d6f135a3cb103e6f4ae8279e55d386ae3a1
Fabian Hirter <mail@fabianhirter.ch>
2023-06-06 20:41:05 +0200

    add validation script

---
+++ Scripts/validate_html.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+validate_html_files() {
+    folder_path=$1
+    html_files=$(find "$folder_path" -type f -name "*.html" -print)
+    css_files=$(find "$folder_path" -type f -name "*.css" -print)
+
+    for file in $html_files; do
+        html_validation_output=$(curl -s -F "uploaded_file=@$file" https://validator.w3.org/nu/?out=json)
+        echo $html_validation_output > output.html
+
+        exit
+
+        if echo "$html_validation_output" | grep -q "The document validates"; then
+            echo "$file is valid"
+        else
+            echo "$file is invalid"
+        fi
+        sleep 2
+    done
+
+    for file in $css_files; do
+
+        css_validation_output=$(curl -s -F "file=@$file" -F "output=json" https://validator.w3.org/nu/?parser=css)
+
+        if echo "$css_validation_output" | grep -q "The document validates"; then
+            echo "$file is valid"
+        else
+            echo "$file is invalid"
+        fi
+        sleep 2
+    done
+}
+
+# Usage example
+folder_path="/Users/fabian/Library/CloudStorage/OneDrive-TEKOSchweizerischeFachschuleAG/_Web_TIN20/Prüfungen/Abgabe1/Abgabe"
+validate_html_files "$folder_path"
