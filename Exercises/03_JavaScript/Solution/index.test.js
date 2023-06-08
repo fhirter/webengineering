3c8742b6f59265850b46f7e16edef87f3c98fece
Fabian Hirter <mail@fabianhirter.ch>
2023-06-08 10:28:56 +0200

    add first integration test

--- Exercises/03_JavaScript/Solution/index.test.js
+++ Exercises/03_JavaScript/Solution/index.test.js
@@ -1,1 +1,19 @@
 import Meteo from "./index";
+
+let meteo;
+beforeEach(function () {
+    meteo = new Meteo();
+});
+
+describe('integration tests', function () {
+    let weather;
+    beforeEach(function () {
+        const address = "Freiburgstrasse 251, 3018 Bern";
+        weather = meteo.getWeather(address);
+    });
+
+    it('should return an object with a temperature property', function () {
+        expect(weather.temperatur).not.toBeUndefined();
+    });
+});
+
