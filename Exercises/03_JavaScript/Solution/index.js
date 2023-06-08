26da07d79a2a04f752b5a13c3192a774426bfb55
Fabian Hirter <mail@fabianhirter.ch>
2023-06-08 10:37:21 +0200

    return weather object

--- Exercises/03_JavaScript/Solution/index.js
+++ Exercises/03_JavaScript/Solution/index.js
@@ -1,6 +1,14 @@
 export default function Meteo() {
     function getWeather(address) {
+        const weather = {
+            humidity: 0,
+            pressure: 0,
+            temperature: 0,
+        }
 
+        return Object.freeze(
+            weather
+        )
     }
 
     return Object.freeze({
