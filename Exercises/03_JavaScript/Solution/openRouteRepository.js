6e31e9391324419a323ba9b78a60d3600e4ebb31
Fabian Hirter <mail@fabianhirter.ch>
2023-06-15 09:43:20 +0200

    make promises more explicit

--- Exercises/03_JavaScript/Solution/openRouteRepository.js
+++ Exercises/03_JavaScript/Solution/openRouteRepository.js
@@ -2,14 +2,14 @@ export default function OpenRouteRepository(apiKey) {
     function getCoordinates(address, callback) {
         const url = `https://api.openrouteservice.org/geocode/search?api_key=${apiKey}&text=${address}`;
 
-        fetch(url)
-            .then((response) => {
-                return response.json()
-            })
-            .then((data) => {
-                const coordinates = extractCoordinates(data);
-                callback(coordinates);
-            })
+        const fetchPromise = fetch(url);
+        const dataPromise = fetchPromise.then((response) => {
+            return response.json()
+        });
+        dataPromise.then((data) => {
+            const coordinates = extractCoordinates(data);
+            callback(coordinates);
+        })
     }
     function extractCoordinates(data) {
         const coordinates = data.features[0].geometry.coordinates;
