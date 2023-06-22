c666aaa458290017af6f7a40940fe2ff189315a5
Fabian Hirter <mail@fabianhirter.ch>
2023-06-22 09:34:42 +0200

    add test suite, check passed object properties

--- Exercises/03_JavaScript/Solution/weather.test.js
+++ Exercises/03_JavaScript/Solution/weather.test.js
@@ -3,40 +3,50 @@ import Weather from "./weather";
 const callback = jest.fn();
 let weatherData
 
-beforeEach(function () {
-    const address = "Freiburgstrasse 251, 3018 Bern";
+describe('Weather data retrieval', function () {
+    beforeEach(function () {
+        const address = "Freiburgstrasse 251, 3018 Bern";
 
-    weatherData = {
-        "temperature": 298.48,
-        "pressure": 1015,
-        "humidity": 64,
-    }
+        weatherData = {
+            "temperature": 298.48,
+            "pressure": 1015,
+            "humidity": 64,
+        }
 
-    const coordinates = {
-        lon: 46.94643551832302,
-        lat: 7.410541652279687
-    }
+        const coordinates = {
+            lon: 46.94643551832302,
+            lat: 7.410541652279687
+        }
 
-    const openWeatherRepositoryMock = {
-        getCurrentWeather: jest.fn((coordinates, callback) => {
-            callback(weatherData);
-        })
-    }
+        const openWeatherRepositoryMock = {
+            getCurrentWeather: jest.fn((coordinates, callback) => {
+                callback(weatherData);
+            })
+        }
 
-    const openRouteRepositoryMock = {
-        getCoordinates: jest.fn((address, callback) => {
-            callback(coordinates);
-        })
-    }
+        const openRouteRepositoryMock = {
+            getCoordinates: jest.fn((address, callback) => {
+                callback(coordinates);
+            })
+        }
 
-    const weather = new Weather(openWeatherRepositoryMock, openRouteRepositoryMock);
-    weather.getWeather(address, callback);
-});
+        const weather = new Weather(openWeatherRepositoryMock, openRouteRepositoryMock);
+        weather.getWeather(address, callback);
+    });
 
-it('should call callback', function () {
-    expect(callback).toHaveBeenCalledTimes(1);
-});
+    it('should call callback', function () {
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
 
-it('should be called with an object that has temperature, pressure and humidity properties', function () {
-    expect(callback).toHaveBeenCalledWith(weatherData);
+    it('should be called with an object that was returned by openWeatherRepository', function () {
+        expect(callback).toHaveBeenCalledWith(weatherData);
+    });
+
+    it('should be called with an object that has temperature, humidity and pressure properties', function () {
+        const argument = callback.mock.calls[0][0];
+
+        expect(argument.temperature).not.toBeUndefined();
+        expect(argument.humidity).not.toBeUndefined();
+        expect(argument.pressure).not.toBeUndefined();
+    });
 });
