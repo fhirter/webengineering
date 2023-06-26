3e17e48a444474f6e7c5d80a82196257751178db
Fabian Hirter <mail@fabianhirter.ch>
2023-06-26 15:57:01 +0200

    add svelte

---
+++ Exercises/03_JavaScript/Solution/config.js
@@ -0,0 +1,4 @@
+export const config = Object.freeze({
+    "openWeatherApiKey": "91d94f615e013d9e0bc0b57920169ac9",
+    "openRouteApiKey": "5b3ce3597851110001cf624832370d1bb44b45349d58c3acecc75181"
+})
