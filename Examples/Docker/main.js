c9aef6f45cdd67f10bca6cfa2abc22d46ac0932c
Fabian Hirter <mail@fabianhirter.ch>
2024-08-21 10:06:34 +0200

    add node app to docker example

---
+++ Examples/Docker/main.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+const port = 8080;
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});app.listen(port, function () {
+    console.log(`Example app listening on http://localhost:${port}.`);
+});
