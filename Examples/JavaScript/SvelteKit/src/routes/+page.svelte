4ebc7f1efe348d1b4083194db3928df50f669eea
Fabian Hirter <mail@fabianhirter.ch>
2024-08-21 11:31:11 +0200

    add svelte kit example

---
+++ Examples/JavaScript/SvelteKit/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+
+    import Foo from "../components/Foo.svelte";
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Foo></Foo>
